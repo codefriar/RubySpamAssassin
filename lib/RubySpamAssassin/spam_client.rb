bd1ee718f5260af0cb30669fd447db697bb0d465
Lukas Satrapa <satrapa.lukas@gmail.com>
2017-05-31 15:06:32 +0200

    Added support for TELL command

--- lib/RubySpamAssassin/spam_client.rb
+++ lib/RubySpamAssassin/spam_client.rb
@@ -17,6 +17,15 @@ class RubySpamAssassin::SpamClient
     result = process_headers protocol_response[0...2]
   end
 
+  def tell(message, message_class, set=true, user=nil)
+    headers = []
+    headers.push("Message-class: #{message_class}")
+    headers.push(set ? "Set: local" : "Remove: local")
+    headers.push("User: #{user}") if !user.nil?
+    protocol_response = send_message("TELL", message, headers)
+    result = process_headers protocol_response[0...2]
+  end
+
   def report(message)
     protocol_response = send_message("REPORT", message)
     result = process_headers protocol_response[0...2]
@@ -41,10 +50,14 @@ class RubySpamAssassin::SpamClient
   alias :process :report
 
   private
-  def send_message(command, message = "")
+
+  def send_message(command, message="", headers=[])
     length = message.bytesize
     @connection_pool.with do |socket|
       socket.write(command + " SPAMC/1.2\r\n")
+      headers.each do |h|
+        socket.write(h + "\r\n")
+      end
       socket.write("Content-length: " + length.to_s + "\r\n\r\n")
       socket.write(message)
       socket.shutdown(1) #have to shutdown sending side to get response
