0f82773b2ab4c8f36a3d1da6d91b6abebc95097e
Fernando Morgenstern <contato@fernandomarcelo.com>
2017-12-13 13:42:06 -0200

    Fix rule texts parsing
    It was using squeeze method without any parameter which will squeeze all characters.
    
    Texts like "Message-Id is not valid, according to RFC 2822" were parsed as "Mesage-Id is not valid, acording to RFC 282". Note that Message-Id was changed to Mesage-Id and RFC 2822 was changed to RFC 282.

--- spec/RubySpamAssassin/report_parser_spec.rb
+++ spec/RubySpamAssassin/report_parser_spec.rb
@@ -6,9 +6,13 @@ describe "ReportParser" do
     result = RubySpamAssassin::ReportParser.parse(spam)
     expect(result.length).to eq(6)
 
-    # Check contents of first one to make sure text/points are formatted correctly
+    # Check contents of some rules to make sure text/points are formatted correctly
     expect(result[0][:pts]).to eq(0.5)
     expect(result[0][:rule]).to eq('DATE_IN_PAST_24_48')
     expect(result[0][:text]).to eq('Date: is 24 to 48 hours before Received: date')
+
+    expect(result[4][:pts]).to eq(1.2)
+    expect(result[4][:rule]).to eq('INVALID_MSGID')
+    expect(result[4][:text]).to eq('Message-Id is not valid, according to RFC 2822')
   end
 end
