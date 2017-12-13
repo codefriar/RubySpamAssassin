3505b1ac4236e44206c6b323b6ad3ca220fd813b
Fernando Morgenstern <contato@fernandomarcelo.com>
2017-12-13 13:39:10 -0200

    Change tests in report_parser_spec.rb to actually check values
    They weren't using expect() so rspec wasn't testing anything.

--- spec/RubySpamAssassin/report_parser_spec.rb
+++ spec/RubySpamAssassin/report_parser_spec.rb
@@ -4,11 +4,11 @@ describe "ReportParser" do
   it "should parse the report text into an informative hash" do
     spam = File.read('spec/data/spam_test1.txt')
     result = RubySpamAssassin::ReportParser.parse(spam)
-    result.length.equal?(6)
+    expect(result.length).to eq(6)
 
     # Check contents of first one to make sure text/points are formatted correctly
-    result[0][:pts].equal?(0.5)
-    result[0][:rule].equal?('DATE_IN_PAST_24_48')
-    result[0][:text].equal?('Date: is 24 to 48 hours before Received: date')
+    expect(result[0][:pts]).to eq(0.5)
+    expect(result[0][:rule]).to eq('DATE_IN_PAST_24_48')
+    expect(result[0][:text]).to eq('Date: is 24 to 48 hours before Received: date')
   end
 end
